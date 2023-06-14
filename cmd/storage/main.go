4cdd5fbe0fabb46916c4510769a4ba540353434a
behryus <behruz.yousupov@gmail.com>
2023-06-14 12:31:30 +0500

    initial commit

---
+++ cmd/storage/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/behryuz/storage/internal/storage"
+	"log"
+)
+
+func main() {
+	st := storage.NewStorage()
+
+	file, err := st.Upload("test.txt", []byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	restoredFile, err := st.GetById(file.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(st.Files)
+
+	fmt.Println("it's restored", restoredFile)
+}
---
+++ pkg/storage/storage.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/behryuz/storage/internal/storage"
+
+func NewStorage() *storage.Storage {
+	return storage.NewStorage()
+}
